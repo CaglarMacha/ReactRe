44bced7511a4b5aa558c190f704e2b0d1274039d
CaglarMacha <caglar.ayhan@boun.edu.tr>
2021-10-30 17:20:38 +0300

    thunk was added

--- src/redux/actions/categoryActions.js
+++ src/redux/actions/categoryActions.js
@@ -1,13 +1,21 @@
-import * as actionTypes from "./actionsTypes"
+import * as actionTypes from "./actionsTypes";
 
 export function changeCategory(category) {
-    return {
-        type:actionTypes.CHANGE_CATEGORY,
-        payload:category
-    } 
+  return {
+    type: actionTypes.CHANGE_CATEGORY,
+    payload: category,
+  };
 }
 
-export function  getCategories(params) {
+export function getCategoriesSuccess(params) {
     
-    
+}
+
+export function getCategories(params) {
+  return function (dispatch) {
+    let url = "http://localhost:3000/categories";
+    return fetch(url)
+      .then((response) => response.json())
+      .then((result) => dispatch(getCategoriesSuccess(result)));
+  };
 }
