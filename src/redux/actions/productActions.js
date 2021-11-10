7f491d08f83d76fde97a4fd78fd0b05dbbbbd70a
CaglarMacha <caglar.ayhan@boun.edu.tr>
2021-11-10 13:48:29 +0300

    Error management  was added

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -17,16 +17,39 @@ export function updateProductSuccess(product) {
   };
 }
 
-export function saveProduct(product) {
+export function saveProductApi(product) {
   return fetch("http://localhost:3000/products/" + (product.id || ""), {
     method: product.id ? "PUT" : "POST",
 
     headers: { "content-type": "application/json" },
-    body:JSON.stringify(product) //Eklenen data budur.
+    body: JSON.stringify(product), //Eklenen data budur.
+  })
+    .then(handleResponse)
+    .catch(handleError);
+}
 
+export function saveProduct(product) {
+  return function (dispatch) {
+    return saveProductApi(product).then((savedProduct) => {
+      product.id
+        ? dispatch(updateProductSuccess(savedProduct))
+        : dispatch(createProductSuccess(savedProduct));
+    }).catch(error=>{throw error});
+  };
+}
 
-    
-  });
+export async function handleResponse(response) {
+    if(response.ok){
+        return response.json()
+    }
+
+    const error = await response.text();
+    throw new Error(error)
+}
+
+export function handleError(error) {
+    console.error("Bir hata oluştu");
+    throw error();
 }
 
 export function getProducts(categoryId) {
